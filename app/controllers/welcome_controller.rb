bb886720a78601be9fdb9e14e391078e0c2d0f8d
Brad Givens <bradgivens22@gmail.com>
2018-07-22 17:53:19 +0100

    First commit - probs too late

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,34 @@
+class WelcomeController < ApplicationController
+  
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def find_project
+  end
+
+  def project_params
+  end
+
+end
